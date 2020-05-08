a5b956a5cc70b52cb2864451ede757ca3dd43c24
Dzsekeb <dzsekeb@gmail.com>
2020-05-08 19:44:56 +0300

    moved credentials out to separate file

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import Telegram from './telegram.js';
 import Database from './database.js';
 import Torrent from './torrent.js';
+import fs from 'fs';
+
+const credentials = JSON.parse(fs.readFileSync("credentials.json"));
 
 const db = new Database();
-const tele = new Telegram('key', db);
+const tele = new Telegram(credentials.apikey, db);
 const qbit = new Torrent("http://localhost:8080", db);
 
 function loop()
